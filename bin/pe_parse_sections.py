4527228dd320ef7cddf33dbc86d000246cfabc0d
Nick Hoffman <nhoffman@mulder.codeword.us>
2014-06-23 13:23:58 -0400

    added pe_parse_sections, very basic code, PoC at best

---
+++ bin/pe_parse_sections.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import pefile
+import sys
+
+pe =  pefile.PE(sys.argv[1], fast_load=True)
+
+info = {}
+
+#logic for the pesections
+info["sections"] = {}
+for section in pe.sections:
+  info["sections"][section.Name] = {}
+  info["sections"][section.Name]["VirtualAddress"] = hex(section.VirtualAddress)
+  info["sections"][section.Name]["Misc_VirtualSize"] = hex(section.Misc_VirtualSize)
+  info["sections"][section.Name]["SizeOfRawData"] = section.SizeOfRawData
