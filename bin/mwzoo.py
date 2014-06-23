d2d5c61cb5d8b003f09cce63283ae5a4b2dafa17
John Davison <JWDavison@ashland.com>
2014-06-23 08:29:07 -0400

    hellow world!

--- bin/mwzoo.py
+++ bin/mwzoo.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
 # vim: ts=4:sw=4:et
 #
 # malware zoo
+#
+
+from twisted.web import server, resource
+from twisted.internet import reactor
+
+class HelloResource(resource.Resource):
+    isLeaf = True
+    numberRequests = 0
+    
+    def render_GET(self, request):
+        self.numberRequests += 1
+        request.setHeader("content-type", "text/plain")
+        return "I am request #" + str(self.numberRequests) + "\n"
+
+
+if __name__ == '__main__':
+    reactor.listenTCP(8081, server.Site(HelloResource()))
+    reactor.run()
