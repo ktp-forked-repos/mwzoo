278ee080b975a6a64a24c55bef8d3edfc9adf321
Coleman Kane <ckane@colemankane.org>
2014-06-24 22:10:11 -0400

    Add comments to describe how to do the thing

--- celery-apps/mwzoo_celery/tasks.py
+++ celery-apps/mwzoo_celery/tasks.py
@@ -5,6 +5,19 @@ from mwzoo_celery.celery import celery
 from celery import group, chord, chain
 import os
 
+#
+# Define each of your tasks here:
+#
+# @celery.task
+# def your_task(args, ...):
+#    your_results = {}
+#    your_results['a'] = do_thing_1()
+#    your_results['b'] = do_other_thing()
+#    do_another_task(your_results).si()
+#    ....
+#    return your_results # will not wait for subtask to finish
+#
+
 @celery.task
 def yara_a_file(filename):
     os.system("yara -g -m -s yara/*.yar '{0}' > scans/'{1}'.scan".format(filename, filename.replace('/','_')))
