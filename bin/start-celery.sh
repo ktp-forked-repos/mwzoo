aa3024663f92604265784a55f575d1f787980d67
Coleman Kane <ckane@colemankane.org>
2014-06-24 22:19:50 -0400

    Add more comments to the celery start-up code

--- bin/start-celery.sh
+++ bin/start-celery.sh
@@ -2,4 +2,13 @@
 #
 # Make sure you run this from inside mwzoo base folder like:
 # ./bin/start-celery.sh
+#
+# "celery worker" tells celery to use the worker module
+# -A mwzoo_celery tells celery to load the "mwzoo_celery" application, which is just a folder name under
+#                 celery-apps. You can divide your codebase up among multiple "applications" by replicating
+#                 the skeleton concepts in that folder.
+# --loglevel=info Set the logging level to INFO
+# -c 5            Spawn 5 worker sub-processes, which work will be distributed across
+#
+
 PYTHONPATH=celery-apps celery worker -A mwzoo_celery --loglevel=info -c 5
