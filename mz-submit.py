f1e02f83e43d79d27fa5fdc42704b4d271648903
John Davison <unixfreak0037@gmail.com>
2014-07-22 14:19:13 -0400

    add option to specify file name on submit

--- mz-submit.py
+++ mz-submit.py
@@ -8,16 +8,16 @@ import base64
 parser = argparse.ArgumentParser(description='MalwareZoo File Submit')
 parser.add_argument(
     '-f', '--input-file', action='store', dest='input_file', required=True,
-    help='The file to upload to the zoo.')
+    help="The file to upload to the zoo.")
 parser.add_argument(
     '-t', '--tags', action='store', nargs="*", dest='tags', required=False, default=[],
-    help='Add the given tags to the sample.')
+    help="Add the given tags to the sample.")
 parser.add_argument(
     '-s', '--sources', action='store', nargs="*", dest='sources', required=False, default=[],
-    help='Add the given sources to the sample.')
-#parser.add_argument(
-    #'--add-new-tags', action='store_true', dest='add_new_tags', required=False, default=False,
-    #help="Add new tags specified with the -t or --tag option.  By default only existing tags can be used (to prevent typo).")
+    help="Add the given sources to the sample.")
+parser.add_argument(
+    '-F', '--with-filename', action='store', dest='file_name', required=False, default=None,
+    help="Set the file name manually (if different than the actual file name.)")
 args = parser.parse_args()
 
 s = xmlrpclib.Server('http://localhost:8081/upload')
@@ -26,10 +26,7 @@ with open(args.input_file, 'rb') as fp:
     file_contents = fp.read()
 
 print s.upload(
-    os.path.basename(args.input_file), 
+    args.file_name if args.file_name is not None else os.path.basename(args.input_file), 
     base64.b64encode(file_contents), 
     args.tags,
-    #args.add_new_tags,
-    args.sources,
-    #args.add_new_sources)
-    )
+    args.sources)
